a9fe433ed52eb0c3591c1c2c560d829900263349
gsim1903 <gavin@internetbusker.co>
2021-11-17 07:00:40 +0100

    cypress added, standard test passed

---
+++ cypress/integration/userCanSeePortfolioStructure.feature.js
@@ -0,0 +1,8 @@
+describe("Portfolio interface",() => {
+    it("sucessfully renders", () =>{
+        cy.visit("http://localhost:3000");
+        cy.get("#header").should("contain", "My Portfolio");
+        cy.get("#footer").should("contain", "Made with React");
+        cy.get("#hello").should("contain", "Good Evening Planet Earth");
+    });
+});
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav>
+            <h2 id="header">My Portfolio</h2>
+        </nav>
+    );
+};
+
+export default Header
