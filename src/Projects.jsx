dfca34e72cf43169acb8745fad0cea003b090051
gsim1903 <gavin@internetbusker.co>
2021-11-18 12:46:33 +0100

    Axios and projects add

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
+
+// import { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 
- const Projects  = () => {
-    return (
-        <Container>
-            <h1 id="projects-header">My Projects</h1>
-            <p>
-            Some of the shizzle Ive been working on 
-            </p>
-        </Container>
-    );
-    
- };
+class Projects extends Component {
+    state = {
+        projects: []
+           
+    }    
+        
 
+
+    render () {
+    const { projects } = this.state;
+
+        let propjectsList = projects.map((project) => {
+            return (
+                <div id={'project-${project.id}'} key = {project.id}>
+                <h3>{project.name}</h3>
+                </div>
+            );
+        });
+        return (
+            <Container>
+            <h1 id="projects-header">My Projects</h1>
+            {propjectsList}
+            </Container>
+        );
+    };
+    
+    componentDidMount() {
+            axios.get("./data/projects.json").then((response) => {
+            this.setState({ projects: response.data });
+            });
+    }
+
+};
+
+//  const Projects  = () => {
+//     return (
+//         <Container>
+//             <h1 id="projects-header">My Projects</h1>
+//             <p>
+//             Some of the shizzle Ive been working on 
+//             </p>
+//         </Container>
+//     );
+    
  export default Projects 
