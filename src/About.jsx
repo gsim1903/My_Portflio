b64ae5bf617e94681897e9ff46348d156a33619d
gsim1903 <gavin@internetbusker.co>
2021-11-19 12:01:48 +0100

    update about section

--- src/About.jsx
+++ src/About.jsx
@@ -6,11 +6,23 @@ import { Container } from "semantic-ui-react";
         <Container>
         <h1 id="about-header">About Me</h1>
 
+        <h2 id="about-sectionA">Music</h2>
         <p>
         Raised in The Highlands of Scotland, Gavin initially picked up the guitar after falling in love with the anthems and melodies of Oasis and the Britpop generation. This opened the doors to their musical influences and led to a fondness of the songwriting talents of The Beatles, Paul Simon and Bob Dylan. 
         Sourfish is Gavin’s guitar driven musical act, he writes and arranges all the songs, plays the guitar and provides an occasional vocal.  The debut EP Time to Play was accompanied with a debut boxing match. The title track was a finalist in The UK Songwriting competition and selected for placement on reality TV. 
         This was followed up with the single “The Sheep are on Fire”, a tie in with leading Scottish football team Aberdeen FC.
         Gavin’s journey to understand and discover new music  includes working as radio DJ promoting breaking music. More recently this has pivoted to a podcast of the stories of other acts, with guest such as Frank Turner, Amanda Palmer and Bear’s Den. 
+        </p>
+
+        <h2 id="about-sectionB">Work</h2>
+        <p>
+        As a former business owner, chartered accountant, finance director, and Big-Four trained auditor, 
+        Gavin, was uniquely placed to spot a gap in the market. He broke free from the traditional accountancy firm offering to assemble a virtual team of financial experts offering clients tailored, hands-on advice and support.
+        The business, Cloud Finance, has a mission statement to provide “Smart Accounting Support”. The principle underlying this is to be an enabler for SME businesses to understand their financial position and drive the levers to support 
+        their business growth and success. 
+        </p>
+        <h2 id="about-sectionB">Now</h2>
+        <p>
         </p>
         </Container>
     );
